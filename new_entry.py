00d2492eb6adb53af9e69c1b55dfe7ef0a814302
newnakashima <newnakashima2014@gmail.com>
2025-12-28 11:42:39 +0900

    images

--- new_entry.py
+++ new_entry.py
@@ -31,6 +31,7 @@ def create_diary_entry(title):
         f.write(f"---\n")
         f.write(f"title: {title}\n")
         f.write(f"date: {date_str}\n")
+        f.write(f"eyecatch: \n")
         f.write(f"tags: []\n")
         f.write(f"---\n\n")
         f.write(f"# {title}\n\n")
